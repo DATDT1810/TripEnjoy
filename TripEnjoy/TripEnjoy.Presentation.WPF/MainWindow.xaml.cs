3ebb444b68dbdd4f7f8517f4196c64308a138abe
MeetHadvani <meet111hadvani@gmail.com>
2024-09-18 00:13:12 -0400

    added the front end Form

--- TripEnjoy/TripEnjoy.Presentation.WPF/MainWindow.xaml.cs
+++ TripEnjoy/TripEnjoy.Presentation.WPF/MainWindow.xaml.cs
@@ -1,24 +1,33 @@
-﻿using System.Text;
+﻿using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace TripEnjoy.Presentation.WPF
+namespace TripApp
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Retrieve form values
+            var startDate = StartDatePicker.SelectedDate;
+            var endDate = EndDatePicker.SelectedDate;
+            var location = LocationTextBox.Text;
+            var peopleCount = PeopleTextBox.Text;
+            var isFlexDate = FlexDateCheckBox.IsChecked ?? false;
+
+            // Display the values
+            MessageBox.Show($"Start Date: {startDate}\n" +
+                            $"End Date: {endDate}\n" +
+                            $"Location: {location}\n" +
+                            $"People: {peopleCount}\n" +
+                            $"Flexible Dates: {isFlexDate}");
+        
+            //From here internet, local connection, cloud based or other database could be attached
+            //and done other functionality.
         }
     }
 }
